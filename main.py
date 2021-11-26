2da1dd8c968a76cc3639c98f218db6978e9f5cd1
Aidan Lakshman <ahl27@pitt.edu>
2021-11-26 11:43:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from corpus import Corpus
 
 ###### USER PARAMETERS #####
 toolname = 'ahl27litreview'
-email = 'pmcsearch@ahl27.com'
+email = 'example@example.com'
 init_pmids = ['24349035']
 verbose = True
 outfile_name = None #'papersfound.txt'
